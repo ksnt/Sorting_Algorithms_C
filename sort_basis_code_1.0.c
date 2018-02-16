0c84a28e2ad5e4db28efc854e8076540fc1d8c04
ksn <ksn0215@gmail.com>
2018-02-17 05:33:12 +0900

    a

--- sort_basis_code_1.0.c
+++ sort_basis_code_1.0.c
@@ -8,11 +8,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 typedef int Item;
 #define key(A) (A)
 #define less(A,B) (key(A) < key(B))
 #define exch(A,B) {Item t = A; A = B; B = t;}
 #define compexch(A,B) if (less (B,A)) exch(A,B)
+
 
 void sort(Item a[], int l, int r)
   {
@@ -22,10 +24,13 @@ void sort(Item a[], int l, int r)
 	compexch(a[j-1],a[j]);
   }
 
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
   {
     int i, N = atoi(argv[1]), sw = atoi(argv[2]); // atoi -> string to int
     int *a = malloc(N*sizeof(int));
+
+    srand((unsigned int)time(NULL));
+
     if(sw)
       for(i=0; i<N; i++)
 	a[i] = 1000 * (1.0*rand()/RAND_MAX);
@@ -34,4 +39,5 @@ main (int argc, char *argv[])
     sort(a,0,N-1);
     for(i=0;i<N;i++) printf("%3d ", a[i]);
     printf("\n");
+    return 0;
   }
