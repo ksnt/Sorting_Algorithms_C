b4b2eac075e5b0ba20d3d4cf264df617a0526801
ksn <ksn0215@gmail.com>
2018-02-17 21:36:43 +0900

    a

---
+++ shell_sort.py
@@ -0,0 +1,32 @@
+# Shell sort
+# ksnt
+# 1.Determine the interval h
+# 2.Apply the insertion sort to the splitted data according to the interval
+# 3.Update the interval, and apply the insertion sort again
+# 4.If the interal h becomes 1, then finally apply the insertion sort to whole 
+
+# The algorithm is very simple but implementation is a little coomplicated
+
+def shell_sort(a_list):
+	sublist_count = len(a_list) // 2 # Now the interval is n/2 where n is the number of elements in the list. The value of sublist_count will be used as gap(=interval) later.
+	while sublist_count > 0:
+		for start_position in range(sublist_count):
+			gap_insertion_sort(a_list,start_position,sublist_count)
+		
+		print("After increments of size", sublist_count, "The list is", a_list)
+
+		sublist_count = sublist_count // 2 # Update the interval
+
+def gap_insertion_sort(a_list,start,gap):
+	for i in range(start + gap, len(a_list), gap):
+		current_value = a_list[i]
+		position = i
+
+		while position>=gap and a_list[position - gap] > current_value:
+			a_list[position] = a_list[position - gap]
+			position = position - gap
+		a_list[position] = current_value
+
+a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+shell_sort(a_list)
+print(a_list)
