fd1a0813a034133def6585d7747e6894087febcd
ksn <ksn0215@gmail.com>
2018-02-17 16:02:14 +0900

    a

--- comp_sp_ds_plot.py
+++ comp_sp_ds_plot.py
@@ -31,16 +31,23 @@ def test4():
 # measure time to execute the code above
 t1 = Timer("test1()", "from __main__ import test1")
 t1_result = t1.timeit(number=1000)
+t1_label = "concat"
 print("concat ",t1_result,"miliseconds")
 t2 = Timer("test2()", "from __main__ import test2")
 t2_result = t2.timeit(number=1000)
+t2_label = "append"
 print("append ",t2_result,"miliseconds")
 t3 = Timer("test3()", "from __main__ import test3")
 t3_result = t3.timeit(number=1000)
+t3_label = "comprehension"
 print("comprehension ",t3_result,"miliseconds")
 t4 = Timer("test4()", "from __main__ import test4")
 t4_result = t4.timeit(number=1000)
+t4_label = "list range"
 print("list range ",t4_result,"miliseconds")
 
-
-plt([t1_result,t2_result,t3_result,t4_result])
+N = 4
+n = range(N)
+plt.bar(n,list([t1_result,t2_result,t3_result,t4_result]))
+plt.xticks(n, list([t1_label,t2_label,t3_label,t4_label]))
+plt.show()
