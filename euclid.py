6d39c9c58fa72277bb2d95ff9247b92ba250aba4
ksn <ksn0215@gmail.com>
2018-02-20 02:42:58 +0900

    a

--- euclid.py
+++ euclid.py
@@ -2,6 +2,12 @@
 # ksnt
 
 def gcd(a,b):
+    #try:
+    #    a % b
+    #except ZeroDivisionError:
+    #    print("Oops!  You should use not 0 for arguments.  Try again...!")
+    if a == 0 or b == 0:
+        return("Do not use 0 for arguments!")
     if a % b == 0:
         print(b)
     else:
